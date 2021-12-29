aab002a14d46b32535ddf0b66bda8837bccb5b20
EdwardJFox <edwardjfox@outlook.com>
2021-12-29 23:36:54 +0000

    Updated gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -115,12 +115,12 @@ module.exports = {
         name: `Microgame Katas`,
         short_name: `GatsbyJS`,
         start_url: `/`,
-        background_color: `#ffffff`,
+        background_color: `#FDFACE`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
+        theme_color: `#FDFACE`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-react-helmet`,
